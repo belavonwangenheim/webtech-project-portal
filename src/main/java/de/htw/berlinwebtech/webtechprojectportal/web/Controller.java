d8bfc3520b15bfa2fb181cd8f4b4edeeb60c77ca
Bela von Wangenheim <belavw@gmx.de>
2024-04-19 23:53:16 +0200

    update Controller

--- src/main/java/de/htw/berlinwebtech/webtechprojectportal/web/Controller.java
+++ src/main/java/de/htw/berlinwebtech/webtechprojectportal/web/Controller.java
@@ -3,8 +3,9 @@ package de.htw.berlinwebtech.webtechprojectportal.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// @RestController ist Hinweis an Spring Boot das HTTP-Anfragen verarbeitet werden
 @RestController
-public class HelloController {
+public class Controller {
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
