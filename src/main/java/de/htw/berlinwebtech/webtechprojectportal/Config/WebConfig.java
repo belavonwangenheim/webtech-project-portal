89915168e1ae340c36fea585df71a04dfaab5396
Bela von Wangenheim <belavw@gmx.de>
2024-05-28 19:58:50 +0200

    update

---
+++ src/main/java/de/htw/berlinwebtech/webtechprojectportal/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package de.htw.berlinwebtech.webtechprojectportal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://tempjob-frontend.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                                .allowedHeaders("*")
+                                .allowCredentials(true);
+            }
+        };
+    }
+}
