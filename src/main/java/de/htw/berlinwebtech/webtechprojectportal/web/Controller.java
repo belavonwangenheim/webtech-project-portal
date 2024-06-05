01dbd5fa089bf73e01c6fc2eea5ededfc632a099
Bela von Wangenheim <belavw@gmx.de>
2024-06-05 23:13:32 +0200

    Added localhost CrossOrigin

--- src/main/java/de/htw/berlinwebtech/webtechprojectportal/web/Controller.java
+++ src/main/java/de/htw/berlinwebtech/webtechprojectportal/web/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://tempjob-frontend.onrender.com")
+@CrossOrigin(origins = {"https://tempjob-frontend.onrender.com", "http://localhost:5173"})
 @RestController
 @RequestMapping("/api/jobs")
 public class Controller {
