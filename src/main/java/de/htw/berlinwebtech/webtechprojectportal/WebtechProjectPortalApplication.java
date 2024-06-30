8f9b1aa5c0bb8050997de484579d534970620c4c
Bela von Wangenheim <belavw@gmx.de>
2024-06-30 21:46:22 +0200

    Update application configuration and add environment variables handling

--- src/main/java/de/htw/berlinwebtech/webtechprojectportal/WebtechProjectPortalApplication.java
+++ src/main/java/de/htw/berlinwebtech/webtechprojectportal/WebtechProjectPortalApplication.java
@@ -1,5 +1,6 @@
 package de.htw.berlinwebtech.webtechprojectportal;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebtechProjectPortalApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
 		SpringApplication.run(WebtechProjectPortalApplication.class, args);
 	}
 
